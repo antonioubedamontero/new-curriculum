652388a41efaa78dca2adac8d1b2bf4668ed8b02
Antonio Ubeda Montero <anubedam@gmail.com>
2025-03-20 09:16:02 +0100

    extract companies to components and resize photo

---
+++ src/app/layout/components/main/components/company-project/company-project.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-project',
+  standalone: false,
+  templateUrl: './company-project.component.html',
+  styleUrl: './company-project.component.scss'
+})
+export class CompanyProjectComponent {
+
+}
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,19 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { AsideComponent } from './components/aside/aside.component';
 import { BaseModule } from '../base/base.module';
+import { CompanyProjectComponent } from './components/main/components/company-project/company-project.component';
+import { CompanySectionComponent } from './components/main/components/company-section/company-section.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LayoutComponent } from './layout.component';
 import { MainComponent } from './components/main/main.component';
 import { TranslateModule } from '@ngx-translate/core';
-import { LayoutComponent } from './layout.component';
 
 @NgModule({
   declarations: [
-    LayoutComponent,
-    HeaderComponent,
-    FooterComponent,
     AsideComponent,
-    MainComponent
+    CompanyProjectComponent,
+    CompanySectionComponent,
+    FooterComponent,
+    HeaderComponent,
+    LayoutComponent,
+    MainComponent,
   ],
   imports: [
     CommonModule,
