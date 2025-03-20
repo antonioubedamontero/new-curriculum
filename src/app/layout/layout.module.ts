6d8cd2670d712063125174d746137e94d0aa235b
Antonio Ubeda Montero <anubedam@gmail.com>
2025-03-20 07:47:38 +0100

    move layout component to base folder

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -5,9 +5,9 @@ import { AsideComponent } from './components/aside/aside.component';
 import { BaseModule } from '../base/base.module';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-import { LayoutComponent } from './components/layout/layout.component';
 import { MainComponent } from './components/main/main.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { LayoutComponent } from './layout.component';
 
 @NgModule({
   declarations: [
