fc2764c90413de84f216777c9a3b43ac8601b278
Antonio Ubeda Montero <anubedam@gmail.com>
2025-03-23 01:27:01 +0100

    increase coverage main component

--- src/app/layout/components/main/main.component.spec.ts
+++ src/app/layout/components/main/main.component.spec.ts
@@ -1,40 +1,151 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { Component, Input } from '@angular/core';
+
+import { MaterialModule } from '../../../base/material/material.module';
 
 import { MainComponent } from './main.component';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { HttpLoaderFactory } from '../../../app.module';
-import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
-import { MaterialModule } from '../../../base/material/material.module';
+import { MainMockService } from '../../../mocks/services/main-mock.service';
+import { MainService } from '../../../service';
+import { MocksModule } from '../../../mocks/mocks.module';
+import { TranslateServiceMock } from '../../../mocks/services/translate-mock.service';
+import { WorkExperienceItemResponse } from '../../../interfaces';
+
+@Component({
+  selector: 'app-company-section',
+  standalone: false,
+  template: '<p>company-section</p>',
+})
+export class CompanySectionMockComponent {
+  @Input() companyExperience!: WorkExperienceItemResponse;
+}
 
 describe('MainComponent', () => {
   let component: MainComponent;
   let fixture: ComponentFixture<MainComponent>;
 
+  let getSummarySpy: any;
+  let getHabilitiesSpy: any;
+  let getTrainingsSpy: any;
+  let getLanguagesSpy: any;
+  let getWebDeveloperExperiencesSpy: any;
+  let getCobolDeveloperExperiencesSpy: any;
+  let getPracticeDeveloperExperiencesSpy: any;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [MainComponent],
+      declarations: [
+        MainComponent,
+        CompanySectionMockComponent
+      ],
       imports: [
-        TranslateModule.forRoot({
-          loader: {
-            provide: TranslateLoader,
-            useFactory: HttpLoaderFactory,
-            deps: [HttpClient],
-          },
-        }),
-        MaterialModule
+        MaterialModule,
+        MocksModule
       ],
       providers: [
-        provideHttpClient(withFetch())  // Configure HttpClient when using with modules
+        provideHttpClient(),  // Configure HttpClient when using with modules
+        provideHttpClientTesting,
+        TranslateServiceMock,
+        {
+          provide: MainService,
+          useClass: MainMockService
+        }
       ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(MainComponent);
     component = fixture.componentInstance;
+
+    getSummarySpy = spyOn(component, 'getSummaryFromServer').and.callThrough();
+    getHabilitiesSpy = spyOn(component, 'getHabilitiesFromServer').and.callThrough();
+    getTrainingsSpy = spyOn(component, 'getTrainingsFromServer').and.callThrough();
+    getLanguagesSpy = spyOn(component, 'getLanguagesFromServer').and.callThrough();
+    getWebDeveloperExperiencesSpy = spyOn(component, 'getWebDeveloperExperiencesFromServer').and.callThrough();
+    getCobolDeveloperExperiencesSpy = spyOn(component, 'getCobolDeveloperExperiencesFromServer').and.callThrough();
+    getPracticeDeveloperExperiencesSpy = spyOn(component, 'getPracticeDeveloperExperiencesFromServer').and.callThrough();
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('- should call this endpoints:', () => {
+    it('get summary', () => {
+      expect(getSummarySpy).toHaveBeenCalled();
+    });
+
+    it('get habilities', () => {
+      expect(getHabilitiesSpy).toHaveBeenCalled();
+    });
+
+    it('get trainings', () => {
+      expect(getTrainingsSpy).toHaveBeenCalled();
+    });
+
+    it('get languages', () => {
+      expect(getLanguagesSpy).toHaveBeenCalled();
+    });
+
+    it('get web developer experiences', () => {
+      expect(getWebDeveloperExperiencesSpy).toHaveBeenCalled();
+    });
+
+    it('get web developer experiences', () => {
+      expect(getWebDeveloperExperiencesSpy).toHaveBeenCalled();
+    });
+
+    it('get cobol developer experiences', () => {
+      expect(getCobolDeveloperExperiencesSpy).toHaveBeenCalled();
+    });
+
+    it('get practice developer experiences', () => {
+      expect(getPracticeDeveloperExperiencesSpy).toHaveBeenCalled();
+    });
+  });
+
+  describe('- should have these sections:', () => {
+    it('summary section', () => {
+      const summarySection = fixture.debugElement.nativeElement.querySelector('.summary-section');
+      expect(summarySection).toBeTruthy();
+    });
+
+    it('habilities section', () => {
+      const habilitiesSection = fixture.debugElement.nativeElement.querySelector('.habilities-section');
+      expect(habilitiesSection).toBeTruthy();
+    });
+
+    it('training section', () => {
+      const trainingSection = fixture.debugElement.nativeElement.querySelector('.training-section');
+      expect(trainingSection).toBeTruthy();
+    });
+
+    it('work experience section', () => {
+      const workExperienceSection = fixture.debugElement.nativeElement.querySelector('.work-experience-section');
+      expect(workExperienceSection).toBeTruthy();
+    });
+
+    it('web developer experience section', () => {
+      const webDeveloperExperienceSection = fixture.debugElement.nativeElement.querySelector('.web-developer-experience');
+      expect(webDeveloperExperienceSection).toBeTruthy();
+    });
+
+    it('cobol experience section', () => {
+      const cobolExperienceSection = fixture.debugElement.nativeElement.querySelector('.cobol-experience');
+      expect(cobolExperienceSection).toBeTruthy();
+    });
+
+    it('practice experience section', () => {
+      const practiceExperienceSection = fixture.debugElement.nativeElement.querySelector('.practice-experience');
+      expect(practiceExperienceSection).toBeTruthy();
+    });
+
+    it('languages section', () => {
+      const languagesSection = fixture.debugElement.nativeElement.querySelector('.languages-section');
+      expect(languagesSection).toBeTruthy();
+    });
   });
 });
--- src/app/service/main.service.spec.ts
+++ src/app/service/main.service.spec.ts
@@ -1,12 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 
-import { combineLatest, defer, of } from 'rxjs';
-
 import { MainService } from '.';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { TranslateServiceMock } from '../mocks/services/translate-mock.service';
-import { habilitiesResponseMock, languagesResponseMock, summaryResponseMock, trainingsResponseMock, workExperiencesResponseMock } from '../mocks/data/main-response-mock';
 
 describe('MainService', () => {
   let service: MainService;
@@ -14,7 +11,6 @@ describe('MainService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        // provideHttpClient(withFetch())  // Configure HttpClient when using with modules
         TranslateServiceMock,
         provideHttpClient(withFetch()),
         provideHttpClientTesting(),
@@ -22,13 +18,13 @@ describe('MainService', () => {
     });
     service = TestBed.inject(MainService);
 
-    spyOn(service, 'getHabilities').and.returnValue(of(habilitiesResponseMock));
-    spyOn(service, 'getLanguages').and.returnValue(of(languagesResponseMock));
-    spyOn(service, 'getPracticeDeveloperWorkExperiences').and.returnValue(of(workExperiencesResponseMock));
-    spyOn(service, 'getSummary').and.returnValue(of(summaryResponseMock));
-    spyOn(service, 'getTrainings').and.returnValue(of(trainingsResponseMock));
-    spyOn(service, 'getCobolDeveloperWorkExperiences').and.returnValue(of(workExperiencesResponseMock));
-    spyOn(service, 'getWebDeveloperWorkExperiences').and.returnValue(of(workExperiencesResponseMock));
+    // spyOn(service, 'getHabilities').and.callThrough();
+    // spyOn(service, 'getLanguages').and.callThrough();
+    // spyOn(service, 'getPracticeDeveloperWorkExperiences').and.callThrough;
+    // spyOn(service, 'getSummary').and.callThrough();
+    // spyOn(service, 'getTrainings').and.callThrough();
+    // spyOn(service, 'getCobolDeveloperWorkExperiences').and.callThrough();
+    // spyOn(service, 'getWebDeveloperWorkExperiences').and.callThrough();
   });
 
   it('should be created', () => {
@@ -36,137 +32,60 @@ describe('MainService', () => {
   });
 
   describe('should retrieve data when request data from endpoints', () =>{
-    it('should retrieve data when call to get habilities endpoint', (done) => {
-      const observableEs = defer(() => {
-          service.language = 'es'; // Cambia el idioma antes de la llamada
-          return service.getHabilities();
-      });
+    it('should retrieve data when call to get habilities endpoint', () => {
+      service.language = 'es'; // Cambia el idioma antes de la llamada
+      expect(service.getHabilities()).toBeTruthy();
 
-      const observableEn = defer(() => {
-        service.language = 'en'; // Cambia el idioma antes de la llamada
-        return service.getHabilities();
-      });
-
-      combineLatest([observableEs, observableEn]).subscribe(observableResponses => {
-        for (const observableResponse of observableResponses) {
-          expect(observableResponse).toBeTruthy();
-        }
-        done();
-      })
+      service.language = 'en'; // Cambia el idioma antes de la llamada
+      expect(service.getHabilities()).toBeTruthy();
     });
 
-    it('should retrieve data when call to get languages endpoint', (done) => {
-      const observableEs = defer(() => {
-          service.language = 'es'; // Cambia el idioma antes de la llamada
-          return service.getLanguages();
-      });
+    it('should retrieve data when call to get languages endpoint', () => {
+      service.language = 'es'; // Cambia el idioma antes de la llamada
+      expect(service.getLanguages()).toBeTruthy();
 
-      const observableEn = defer(() => {
-        service.language = 'en'; // Cambia el idioma antes de la llamada
-        return service.getLanguages();
-      });
-
-      combineLatest([observableEs, observableEn]).subscribe(observableResponses => {
-        for (const observableResponse of observableResponses) {
-          expect(observableResponse).toBeTruthy();
-        }
-        done();
-      })
+      service.language = 'en'; // Cambia el idioma antes de la llamada
+      expect(service.getLanguages()).toBeTruthy();
     });
 
-    it('should retrieve data when call to get practice developer work experiences endpoint', (done) => {
-      const observableEs = defer(() => {
-          service.language = 'es'; // Cambia el idioma antes de la llamada
-          return service.getPracticeDeveloperWorkExperiences();
-      });
+    it('should retrieve data when call to get practice developer work experiences endpoint', () => {
+      service.language = 'es'; // Cambia el idioma antes de la llamada
+      expect(service.getPracticeDeveloperWorkExperiences()).toBeTruthy();
 
-      const observableEn = defer(() => {
-        service.language = 'en'; // Cambia el idioma antes de la llamada
-        return service.getPracticeDeveloperWorkExperiences();
-      });
-
-      combineLatest([observableEs, observableEn]).subscribe(observableResponses => {
-        for (const observableResponse of observableResponses) {
-          expect(observableResponse).toBeTruthy();
-        }
-        done();
-      })
+      service.language = 'en'; // Cambia el idioma antes de la llamada
+      expect(service.getPracticeDeveloperWorkExperiences()).toBeTruthy();
     });
 
-    it('should retrieve data when call to get summary endpoint', (done) => {
-      const observableEs = defer(() => {
-          service.language = 'es'; // Cambia el idioma antes de la llamada
-          return service.getSummary();
-      });
+    it('should retrieve data when call to get summary endpoint', () => {
+      service.language = 'es'; // Cambia el idioma antes de la llamada
+      expect(service.getSummary()).toBeTruthy();
 
-      const observableEn = defer(() => {
-        service.language = 'en'; // Cambia el idioma antes de la llamada
-        return service.getSummary();
-      });
-
-      combineLatest([observableEs, observableEn]).subscribe(observableResponses => {
-        for (const observableResponse of observableResponses) {
-          expect(observableResponse).toBeTruthy();
-        }
-        done();
-      })
+      service.language = 'en'; // Cambia el idioma antes de la llamada
+      expect(service.getSummary()).toBeTruthy();
     });
 
-    it('should retrieve data when call to get trainings endpoint', (done) => {
-      const observableEs = defer(() => {
-          service.language = 'es'; // Cambia el idioma antes de la llamada
-          return service.getTrainings();
-      });
+    it('should retrieve data when call to get trainings endpoint', () => {
+      service.language = 'es'; // Cambia el idioma antes de la llamada
+      expect(service.getTrainings()).toBeTruthy();
 
-      const observableEn = defer(() => {
-        service.language = 'en'; // Cambia el idioma antes de la llamada
-        return service.getTrainings();
-      });
-
-      combineLatest([observableEs, observableEn]).subscribe(observableResponses => {
-        for (const observableResponse of observableResponses) {
-          expect(observableResponse).toBeTruthy();
-        }
-        done();
-      })
+      service.language = 'en'; // Cambia el idioma antes de la llamada
+      expect(service.getTrainings()).toBeTruthy();
     });
 
-    it('should retrieve data when call to get Cobol developer experience endpoint', (done) => {
-      const observableEs = defer(() => {
-          service.language = 'es'; // Cambia el idioma antes de la llamada
-          return service.getCobolDeveloperWorkExperiences();
-      });
+    it('should retrieve data when call to get Cobol developer experience endpoint', () => {
+      service.language = 'es'; // Cambia el idioma antes de la llamada
+      expect(service.getCobolDeveloperWorkExperiences()).toBeTruthy();
 
-      const observableEn = defer(() => {
-        service.language = 'en'; // Cambia el idioma antes de la llamada
-        return service.getCobolDeveloperWorkExperiences();
-      });
-
-      combineLatest([observableEs, observableEn]).subscribe(observableResponses => {
-        for (const observableResponse of observableResponses) {
-          expect(observableResponse).toBeTruthy();
-        }
-        done();
-      })
+      service.language = 'en'; // Cambia el idioma antes de la llamada
+      expect(service.getCobolDeveloperWorkExperiences()).toBeTruthy();
     });
 
-    it('should retrieve data when call to get Web developer experience endpoint', (done) => {
-      const observableEs = defer(() => {
-          service.language = 'es'; // Cambia el idioma antes de la llamada
-          return service.getWebDeveloperWorkExperiences();
-      });
+    it('should retrieve data when call to get Web developer experience endpoint', () => {
+      service.language = 'es'; // Cambia el idioma antes de la llamada
+      expect(service.getWebDeveloperWorkExperiences()).toBeTruthy();
 
-      const observableEn = defer(() => {
-        service.language = 'en'; // Cambia el idioma antes de la llamada
-        return service.getWebDeveloperWorkExperiences();
-      });
-
-      combineLatest([observableEs, observableEn]).subscribe(observableResponses => {
-        for (const observableResponse of observableResponses) {
-          expect(observableResponse).toBeTruthy();
-        }
-        done();
-      })
+      service.language = 'en'; // Cambia el idioma antes de la llamada
+      expect(service.getWebDeveloperWorkExperiences()).toBeTruthy();
     });
   });
 });
